82bd690e35993e9cbd485c24e5998033bc7e4e67
Matias Benary <matiasbenary@gmai.com>
2021-01-31 19:02:47 -0300

    :bug: Errores de typescript

--- src/components/shared/Counter/index.tsx
+++ src/components/shared/Counter/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const index = ({ count, setCount }:{count: number, setCount:(number:number)=>null}) => {
+const index = ({ count, setCount }:{count: number, setCount:any}) => {
   return (
         <div className="flex items-center my-3">
                <button onClick={() => setCount(count - 1)} className="flex items-center justify-center px-2 py-1 border border-transparent text-base font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700 mr-2"> - </button>
